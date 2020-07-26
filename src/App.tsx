80f00fb0cbbc9aa3013ba24c26af21676a4bb38c
Fred <frf@qbere.com>
2020-07-26 09:39:53 +0200

    msnodesqlv8 added error cppDriver.Connection is not a constructor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { SqlClient } from "msnodesqlv8";
 
+const connectionString =
+  "server=DB-SRS2-TEST;Database=qbere;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT name FROM deprod.ct_country";
 export const App = () => {
-  return <h1>Solvency II</h1>;
+  const [list, setList] = useState<string[]>([]);
+  useEffect(() => {
+    const sql: SqlClient = require("msnodesqlv8");
+    sql.open(connectionString, (err, con) => {
+      if (err) {
+        console.error(err);
+      } else {
+        con.query(query, (err, rows) => {
+          if (rows) {
+            console.log(rows);
+            setList(rows.map((r) => r.name));
+          }
+        });
+      }
+    });
+  });
+  return (
+    <>
+      <h1>Solvency II</h1>
+      {list.map((l) => (
+        <p key={l}>l</p>
+      ))}
+    </>
+  );
 };
